2aacfa6bf0512d2f67e6575e47518eb9a3d24aa8
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-12-23 15:48:17 +0900

    fix: html에 합침

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const number = document.querySelector(".num");
 
-function onClick(range) {
-    const randomNumber = Math.floor(Math.random() * range * 10) + 1;
-    number.innerText = randomNumber;
-}
